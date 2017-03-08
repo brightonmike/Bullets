1ed266fc355ab8a90a1e6bee7d111c385370806e
Michael Gunner <michael@neptik.com>
2017-03-08 12:01:25 +0000

    fix a few issues with the grid/wp-caption

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
+<article id="post-<?php the_ID(); ?>" <?php post_class('content'); ?> role="article">					
 	<header>
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 		<?php get_template_part( 'parts/content', 'byline' ); ?>
--- parts/loop-single.php
+++ parts/loop-single.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+<article id="post-<?php the_ID(); ?>" <?php post_class('content'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
 	<header>	
 		<h1 itemprop="headline"><?php the_title(); ?></h1>
