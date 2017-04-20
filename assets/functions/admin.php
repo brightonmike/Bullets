405969ed4bf6457a4bbf3fd33f92a0c20115a64e
Michael Gunner <michael@neptik.com>
2017-04-20 09:23:50 +0100

    some tidy up, deferred js, jquery in footer, couple of sass tweaks

--- assets/functions/admin.php
+++ assets/functions/admin.php
@@ -1,9 +1,7 @@
 <?php
-/************* CUSTOMIZE ADMIN *******************/
-// Custom Backend Footer
+
 function bullets_custom_admin_footer() {
-	_e('<span id="footer-thankyou">Developed by <a href="#" target="_blank">Your Site Name</a></span>.', 'bullets');
+	_e('<div id="footer-thankyou">Developed by <a href="#" target="_blank">Your Site Name</a></div>.', 'bullets');
 }
 
-// adding it to the admin area
 add_filter('admin_footer_text', 'bullets_custom_admin_footer');
--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -28,25 +28,6 @@ class Topbar_Menu_Walker extends Walker_Nav_Menu {
     }
 }
 
-// The Off Canvas Menu
-function bullets_off_canvas_nav() {
-	 wp_nav_menu(array(
-        'container' => false,                           // Remove nav container
-        'menu_class' => 'vertical menu',       // Adding custom nav class
-        'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
-        'theme_location' => 'main-nav',        			// Where it's located in the theme
-        'depth' => 5,                                   // Limit the depth of the nav
-        'fallback_cb' => false,                         // Fallback function (see below)
-        'walker' => new Off_Canvas_Menu_Walker()
-    ));
-} 
-
-class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
-    function start_lvl(&$output, $depth = 0, $args = Array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class=\"vertical menu\">\n";
-    }
-}
 
 // The Footer Menu
 function bullets_footer_links() {
@@ -73,12 +54,8 @@ function bullets_main_nav_fallback() {
 	) );
 }
 
-// Footer Fallback Menu
-function bullets_footer_links_fallback() {
-	/* You can put a default here if you like */
-}
 
-// Add Foundation active class to menu
+// Add active class to menu
 function required_active_nav_class( $classes, $item ) {
     if ( $item->current == 1 || $item->current_item_ancestor == true ) {
         $classes[] = 'active';
